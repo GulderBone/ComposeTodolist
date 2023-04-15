143a0d503ab3bcfd825b024f439de6dc8eded135
Mateusz Bartkowiak <mbartkowiak@spoton.com>
2023-04-15 13:49:38 +0200

    linethrough

--- app/src/main/java/com/gulderbone/todolist/screens/todolist/TodoItem.kt
+++ app/src/main/java/com/gulderbone/todolist/screens/todolist/TodoItem.kt
@@ -1,7 +1,5 @@
 package com.gulderbone.todolist.screens.todolist
 
-import android.content.res.Resources.Theme
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -11,10 +9,10 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.gulderbone.todolist.data.Todo
 
@@ -24,22 +22,22 @@ fun TodoItem(
     onEvent: (TodoListEvent) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val background = if (todo.isDone) {
-        MaterialTheme.colorScheme.inverseSurface
+    val textDecoration = if (todo.isDone) {
+        TextDecoration.LineThrough
     } else {
-        MaterialTheme.colorScheme.surface
+        TextDecoration.None
     }
     Row(
-        modifier = modifier
-            .background(background),
+        modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Text(
             modifier = Modifier
                 .padding(horizontal = 8.dp)
                 .weight(0.8f),
             text = todo.title,
+            textDecoration = textDecoration,
         )
         Checkbox(
             checked = todo.isDone,
